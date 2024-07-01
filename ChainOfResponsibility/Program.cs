22fc89ff4924bdde930b84f933ab78d2db0cb56c
Kaloyan Drenski <drenski666@gmail.com>
2024-07-01 17:19:13 +0300

    Add project files.

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,95 @@
+﻿public class Document { }
+
+public abstract class DocumentHandler
+{
+    public DocumentHandler _next;
+
+    protected DocumentHandler(DocumentHandler next)
+    {
+        _next = next;
+    }
+
+    public void SetNextHandler(DocumentHandler next)
+    {
+        _next = next;
+    }
+
+    public abstract void HandleRequest(Document document);
+}
+
+public class GrammarCheckHandler : DocumentHandler
+{
+    public GrammarCheckHandler(DocumentHandler next) : base(next) { }
+
+    public override void HandleRequest(Document document)
+    {
+        Console.WriteLine("Checking for grammar mistakes...");
+
+        if (_next != null)
+        {
+            _next.HandleRequest(document);
+        }
+    }
+}
+
+public class GDPRComplianceCheckHandler : DocumentHandler
+{
+    public GDPRComplianceCheckHandler(DocumentHandler next) : base(next) { }
+
+    public override void HandleRequest(Document document)
+    {
+        Console.WriteLine("Checking for GDPR compliance...");
+
+        if (_next != null)
+        {
+            _next.HandleRequest(document);
+        }
+    }
+}
+
+public class CopyrightInfringementCheckHandler : DocumentHandler
+{
+    public CopyrightInfringementCheckHandler(DocumentHandler next) : base(next) { }
+
+    public override void HandleRequest(Document document)
+    {
+        Console.WriteLine("Checking for copyright infrigement...");
+
+        if (_next != null)
+        {
+            _next.HandleRequest(document);
+        }
+    }
+}
+
+public class SensitiveInformationCheckHandler : DocumentHandler
+{
+    public SensitiveInformationCheckHandler(DocumentHandler next) : base(next) { }
+
+    public override void HandleRequest(Document document)
+    {
+        Console.WriteLine("Checking for sensitive information (passwords, certificates, etc.)...");
+
+        if (_next != null)
+        {
+            _next.HandleRequest(document);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Document document = new Document();
+
+        // Chain the handlers in the correct order
+        DocumentHandler handlerChain = new GrammarCheckHandler(
+            new GDPRComplianceCheckHandler(
+                new CopyrightInfringementCheckHandler(
+                    new SensitiveInformationCheckHandler(null))));
+
+        // Start the processing with the first handler
+        handlerChain.HandleRequest(document);
+    }
+}
---
+++ Decorator Pattern/Program.cs
@@ -0,0 +1,75 @@
+﻿public abstract class IceCream
+{
+    protected string _description = "Ice Cream";
+    public abstract double Cost();
+    public string Description
+    {
+        get { return _description; }
+    }
+}
+
+public class VanillaIceCream : IceCream
+{
+    public override double Cost()
+    {
+        return 1.0;
+    }
+}
+
+public class ChocolateIceCream : IceCream
+{
+    public override double Cost()
+    {
+        return 1.5;
+    }
+}
+
+public abstract class IceCreamDecorator : IceCream
+{
+    protected IceCream _iceCream;
+    public IceCreamDecorator(IceCream iceCream)
+    {
+        _iceCream = iceCream;
+    }
+    public override double Cost()
+    {
+        return _iceCream.Cost();
+    }
+}
+
+public class NutsDecorator : IceCreamDecorator
+{
+    public NutsDecorator(IceCream iceCream) : base(iceCream)
+    {
+        _description += ", Nuts";
+    }
+    public override double Cost()
+    {
+        return _iceCream.Cost() + 0.2;
+    }
+}
+
+public class SprinklesDecorator : IceCreamDecorator
+{
+    public SprinklesDecorator(IceCream iceCream) : base(iceCream)
+    {
+        _description += ", Sprinkles";
+    }
+    public override double Cost()
+    {
+        return _iceCream.Cost() + 0.1;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IceCream iceCream = new VanillaIceCream();
+        Console.WriteLine(iceCream.Cost());
+
+        iceCream = new NutsDecorator(new SprinklesDecorator(new SprinklesDecorator(new SprinklesDecorator(new NutsDecorator(new SprinklesDecorator(new SprinklesDecorator((iceCream))))))));
+        Console.WriteLine(iceCream.Cost());
+        Console.WriteLine(iceCream.Description);
+    }
+}
